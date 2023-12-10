1682e01238d2c6a2ee19690118ddbd1236bc6011
Justine Le Nautout <lenautout.justine@gmail.com>
2023-12-10 22:17:11 +0100

    add scene menu

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    // Lorsque l'on clique sur le bouton, cela lance le jeu.
+    public void OnButtonClickJouer(){
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Lorsque l'on clique sur le bouton, cela quitte l'application
+    public void OnButtonClickQuitter(){
+        Application.Quit();
+    }
+}

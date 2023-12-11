7f2691a748308ae6536b31b4e2e598859c753723
Justine Le Nautout <lenautout.justine@gmail.com>
2023-12-11 11:07:40 +0100

    add touch ennemi

---
+++ Assets/Scripts/ennemiTouch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnnemiTouch : MonoBehaviour
+{
+    // Instanciation des variables
+    public GameObject Ballon;
+
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Fonction lors de collision 
+    private void OnCollisionEnter(Collision other) 
+    {
+        // Collision avec un obus
+        if(other.gameObject.tag == "Ballon")
+        {
+            //Le gameObject est ensuite détruit après avoir été remplacer par un tank cassé.
+            Destroy(gameObject);
+        }
+    }
+}

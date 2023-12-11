741eadcf5e5765ea042be7321ffdb889b738c4ab
JPreault <99177462+JPreault@users.noreply.github.com>
2023-12-11 13:50:50 +0100

    ajout des condition de fin

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeRemaining = 120;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI timeText;
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            GameObject[] go = GameObject.FindGameObjectsWithTag("Soccer");
+            Scene currentScene = SceneManager.GetActiveScene ();
+            if(currentScene.name == "SampleScene" && go.Length == 0){
+                //Loose
+                SceneManager.LoadScene("MenuPrincipal");
+            }
+
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                //Win
+                SceneManager.LoadScene("MenuPrincipal");
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}

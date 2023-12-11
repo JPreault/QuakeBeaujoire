6022effba381a8388933674b32015b903e8f754d
JPreault <99177462+JPreault@users.noreply.github.com>
2023-12-11 09:55:23 +0100

    add ennemy script

---
+++ Assets/Scripts/Ballon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballon : MonoBehaviour
+{
+    // Fonction lors de collision.
+    private void OnCollisionEnter(Collision other) {
+
+        // Destruction du ballon lors d'une collision
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TirJoueur.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TirJoueur : MonoBehaviour
+{
+    public GameObject Ballon;
+    public Transform joueur;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Lorsque le joueur clique sur la touche E ou le clique gauche de la souris, le joueur tir.
+        if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Mouse0)) {
+            GameObject goBallon = Instantiate(Ballon, joueur.position, joueur.rotation);
+            Rigidbody rbTir = goBallon.GetComponent<Rigidbody>();
+            rbTir.velocity = 20f * joueur.forward;
+        }
+    }
+}
